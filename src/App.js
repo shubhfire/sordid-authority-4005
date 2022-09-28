d78842ea86cdce7ae5aadfab45164370c0d5f59c
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-09-28 17:47:32 +0530

    lend page data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import LendPage from "./Pages/LendPage/LendPage";
 
 function App() {
   return (
     <div className="App">
-     
+      <LendPage />
     </div>
   );
 }
---
+++ src/Pages/LendPage/LendPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const LendPage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default LendPage;
