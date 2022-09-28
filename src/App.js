866bdeae1f9bb3495baf62ffeb3bb26c1ea3a01c
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-09-28 18:09:09 +0530

    lend page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import LendPage from "./Pages/LendPage/LendPage";
 
 function App() {
   return (
     <div className="App">
-      <LendPage />
+      
     </div>
   );
 }
--- src/Pages/LendPage/LendPage.jsx
+++ src/Pages/LendPage/LendPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-
 const LendPage = () => {
   return (
     <div>
-      
+      <div className="lendHeading">
+        <h1>You can help a family fundraise for a better life. Make a loan.</h1>
+        <p>
+          Select a loan you feel the most for, based on the cause, geography or
+          the fundraising need.
+        </p>
+      </div>
     </div>
   );
 };
