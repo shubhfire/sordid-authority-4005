f372031aaf547277cb7cd61f32d2d3683a4ee794
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-09-29 19:43:25 +0530

    update in lend page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import './App.css';
-import Navbar from './Components/Navbar';
-import AllRoutes from './Pages/AllRoutes';
+import "./App.css";
+import LendPage from "./Pages/LendPage/LendPage";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <AllRoutes/>
+      {/* <Navbar/>
+     <AllRoutes/> */}
+      <LendPage />
     </div>
   );
 }
