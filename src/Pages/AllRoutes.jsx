e1e89e3aab69217bae03c853f2c8e33bb3f5e41f
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-10-02 14:45:59 +0530

    last update

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,33 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
-// import Login from '../loginPage/Login'
-// import Login from '../loginPage/Login'
-// import Register from '../registerPage/Register'
-
-
-// import LendPage from './LendPage'
-
-
 import ContactUs from '../Pages/ContactUs/ContactUs'
-
-
-
-// import HomePage from '../Pages/HomePage'
 import Donate from './Donate'
 import SingleDonate from '../Components/Donate/SingleDonate'
 import SuccessPayment from './SuccessPayment'
-
-
-
-
-// import ContactUs from "../Pages/ContactUs/ContactUs";
 import Price from "./Pricing/Price";
-// import Price from "./Pricing/Price";
 import HomePage from "../Pages/HomePage";
 import Register from "../Pages/registerPage/Register"
 import OTPPage from '../Pages/registerPage/OTPPage';
 import Login from "../Pages/loginPage/Login"
+// import LendPage from "./LendPage/LendPage"
 
 
 const AllRoutes = () => {
@@ -35,37 +17,17 @@ const AllRoutes = () => {
     <div>
 
     <Routes>
-
-        {/* <Route path="/" element={<HomePage/>}/> */}
-
-
         <Route path="/" element={<HomePage/>}/>
         <Route path="/donate" element={<Donate />} />
         <Route path="/donate/:id" element={<SingleDonate />} />
-
         <Route path='/contactUs' element={<ContactUs/>}/>
-
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
-
-        {/* <Route path='/pricing' element={<Pricing/>}/> */}
-
-        {/* <Route path='/contactUs' element={<ContactUs/>}/> */}
+        <Route path='/OTPPage' element={<OTPPage/>}/> 
+        <Route path="/pricing" element={<Price />} />
+        {/* <Route path="/fund" element={<LendPage />} /> */}
         <Route path='/SuccessPayment' element={<SuccessPayment />}/>
     </Routes>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-
-
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/OTPPage' element={<OTPPage/>}/> 
-        <Route path="/contactUs" element={<ContactUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pricing" element={<Price />} />
-
-
-      </Routes>
     </div>
   );
 };
